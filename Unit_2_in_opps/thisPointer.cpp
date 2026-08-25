5388a9490f512a204ae96057ac6b11e50d86356b
Vansh Aggarwal <vanshagg7002@gmail.com>
2026-08-25 11:01:13 +0530

    this pointer and chaning method

---
+++ Unit_2_in_opps/thisPointer.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Employee {
+    private:
+    int id;
+    public setId (int id) {
+        id = id;
+        this-> id = id;
+    }
+};
+
+class Box {
+    private:
+    double width = 0, height = 0;
+    public:
+    Box& setWidth(double w) {
+        width = w;
+        return *this;
+    }
+    Box& setHeight(double h) {
+        height = h;
+        return *this;
+    }
+};
+
+int main () {
+    Box myBox;
+    myBox.setWidth(10.5).setHeight(5.2);
+
+    
+}
